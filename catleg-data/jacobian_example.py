26bc3ca8a7d6756b0ff36cac98af84e08e2268d9
Hörður Freyr Yngvason <hordurf@gmail.com>
2014-03-18 17:06:51 -0700

    jacobian for a planar three joint system

--- catleg-data/jacobian_example.py
+++ catleg-data/jacobian_example.py
@@ -1,29 +1,31 @@
-# ;; fn      :endpoint
-# ;; inputs  :q angles between joints, where q1 is the furthest point from the endpoint
-# ;; outputs : matrix
-def endpoint(q, L):
-	import sympy
-	import numpy
-	L1, L2, L3, q1, q2, q3 = sympy.symbols('L1 L2 L3 q1 q2 q3')
-	def jacobian(xs, symbols):
-	    return sympy.Matrix([[sympy.diff(x, s) for s in symbols] for x in xs] )
-	    
-	G_x = L1 * sympy.cos(q1) + L2*sympy.cos(q1 + q2) + L3 * sympy.cos(q1+q2+q3)
-	G_y = L1 * sympy.sin(q1) + L2*sympy.sin(q1 + q2) + L3 * sympy.sin(q1+q2+q3)
-	G_alpha = q1 + q2 + q3
-	x = [G_x, G_y, G_alpha]
-	q = [q1, q2, q3]
-	symbolic_J = jacobian(x, q)
+import sympy as sympy
+import numpy as numpy
 
+def symbolic_jacobian(functions, variables):
+    """
+    Compute the symbolic jacobian of the symbolic expression vector 
+    ``functions`` with respect to the symbolic variables in the ``variables`` 
+    vector
+    """
+    return sympy.Matrix([[sympy.diff(f, x) for x in variables] for f in functions])
 
-	J_evalf = symbolic_J.evalf(subs={q1:q[1], q2:q[2], q3:q[3], L1:L[1],L2:L[2], L3:L[3]})
-	# now get the jacobian inverse
+def planar_three_joint_jacobian(q, L):
+    cos = sympy.cos
+    sin = sympy.sin
+ 
+    L1, L2, L3, q1, q2, q3 = sympy.symbols('L1 L2 L3 q1 q2 q3')
+    G_x = L1 * cos(q1) + L2*cos(q1 + q2) + L3 * cos(q1+q2+q3)
+    G_y = L1 * sin(q1) + L2*sin(q1 + q2) + L3 * sin(q1+q2+q3)
+    G_alpha = q1 + q2 + q3
+    x = [G_x, G_y, G_alpha]
+    q_symbolic = [q1, q2, q3]
 
-	J_inv_evalf = J_evalf.inv()
-	J_inv_numpy = numpy.matrix(J_inv_evalf).astype(numpy.double)
-	J_inv_numpy # this is a numpy matrix now
+    J = symbolic_jacobian(x, q_symbolic)
 
-	J_inv_numpy = numpy.linalg.inv(J_evalf_numpy)
-	return J_inv_numpy.transpose()
+    subs = {q1:q[0], q2:q[1], q3:q[2], 
+            L1:L[0], L2:L[1], L3:L[2]}
+    J_numpy = numpy.matrix(J.evalf(subs = subs)).astype(float)
 
-endpoint(q=[20, 30, 40], L=[25, 15, 10])
+    return numpy.linalg.inv(J_numpy).transpose()
+    
+_J = planar_three_joint_jacobian(q = [20, 30, 40], L = [25, 15, 10])
